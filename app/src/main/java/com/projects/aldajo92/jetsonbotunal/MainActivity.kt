a03d5609fe8d996df79b825fd370799f2d41a5f4
aldajo92 <alejodiscovery20@gmail.com>
2021-06-26 00:18:38 -0500

    added a basic functional application with socket.io

--- app/src/main/java/com/projects/aldajo92/jetsonbotunal/MainActivity.kt
+++ app/src/main/java/com/projects/aldajo92/jetsonbotunal/MainActivity.kt
@@ -2,10 +2,65 @@ package com.projects.aldajo92.jetsonbotunal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.gson.Gson
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var mSocket: Socket
+    private val userName: String = "hELLO"
+    private val roomName: String = "ROOM"
+
+    private val objectGSon by lazy { Gson() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        send.setOnClickListener(this)
+
+        try {
+            mSocket = IO.socket("http://192.168.0.245:6666")
+            Log.d("success", mSocket.id())
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("fail", "Failed to connect")
+        }
+
+        mSocket.connect()
+        mSocket.on(Socket.EVENT_CONNECT, connectedEvent)
+        mSocket.on("robot-message", robotMessage)
+    }
+
+    var connectedEvent = Emitter.Listener {
+        Log.i("ADJGF_TAG", "connected")
+    }
+
+    var robotMessage = Emitter.Listener {
+        Log.i("ADJGF_TAG", Arrays.toString(it))
+    }
+
+    private fun sendMessage() {
+        val content = "hello"
+        val sendData = SendMessage(userName, content, roomName)
+        val jsonData = objectGSon.toJson(sendData)
+        mSocket.emit("newMessage", jsonData)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0!!.id) {
+            R.id.send -> sendMessage()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mSocket.disconnect()
     }
 }
---
+++ app/src/main/java/com/projects/aldajo92/jetsonbotunal/Models.kt
@@ -0,0 +1,5 @@
+package com.projects.aldajo92.jetsonbotunal
+
+data class Message (val userName : String, val messageContent : String, val roomName: String,var viewType : Int)
+data class initialData (val userName : String, val roomName : String)
+data class SendMessage(val userName : String, val messageContent: String, val roomName: String)
