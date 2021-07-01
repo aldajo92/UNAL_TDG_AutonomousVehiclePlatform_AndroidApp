42249cf72fd613e0e0ce860c5b9fd0f6fe86f419
Alejandro Gómez <alejodiscovery20@gmail.com>
2021-06-30 23:38:59 -0500

    Mjpeg viewer (#2)
    * graph feature working correctly with socketio
    
    * Improved implementation to handle the data provided by socket
    
    * improved graphs
    
    * improved realtimeWrapper and VideoStreaming view

--- app/src/main/java/com/projects/aldajo92/jetsonbotunal/Models.kt
+++ app/src/main/java/com/projects/aldajo92/jetsonbotunal/Models.kt
@@ -4,12 +4,7 @@ data class RobotVelocityEncoder(
     val velocityEncoder: Float
 )
 
-data class RobotImu(
-    val velocity: Float,
-    val magnet: Float
-)
-
 data class MoveRobotMessage(
-    val steeringValue: String,
-    val throttle: String
+    val steering: Float,
+    val throttle: Float
 )
--- app/src/main/java/com/projects/aldajo92/jetsonbotunal/SocketManager.kt
+++ app/src/main/java/com/projects/aldajo92/jetsonbotunal/SocketManager.kt
@@ -13,7 +13,7 @@ class SocketManager(private val socketListener: SocketListener) {
 
     fun connect() {
         try {
-            mSocket = IO.socket(BASE_URL)
+            mSocket = IO.socket(DATA_SOCKET_PATH)
             Log.d("success", mSocket.id())
         } catch (e: Exception) {
             e.printStackTrace()
@@ -36,7 +36,7 @@ class SocketManager(private val socketListener: SocketListener) {
 
     fun sendData(src: Any) {
         val jsonData = objectGSon.toJson(src)
-        mSocket.emit("newMessage", jsonData)
+        mSocket.emit(ROBOT_COMMAND, jsonData)
     }
 
     fun disconnect() {
