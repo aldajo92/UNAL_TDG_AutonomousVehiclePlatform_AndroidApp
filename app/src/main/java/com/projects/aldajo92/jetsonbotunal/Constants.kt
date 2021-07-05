dd9e88a2d74cf5e5e5d85b8fd8e68f6177c58884
aldajo92 <alejodiscovery20@gmail.com>
2021-07-04 19:02:47 -0500

    added breaklines

--- app/src/main/java/com/projects/aldajo92/jetsonbotunal/Constants.kt
+++ app/src/main/java/com/projects/aldajo92/jetsonbotunal/Constants.kt
@@ -3,17 +3,6 @@ package com.projects.aldajo92.jetsonbotunal
 const val ROBOT_MESSAGE = "robot-message"
 const val ROBOT_COMMAND = "robot-command"
 
-//const val BASE_URL = "http://192.168.0.123"
-//const val DATA_SOCKET_PATH = "$BASE_URL:5170"
-//const val VIDEO_STREAMING_PATH = "$BASE_URL:8080/stream?topic=/camera_processing/camera/image_color/BGR/raw"
-
-//const val BASE_URL = "https://jetsonbotunal.ngrok.io"
-//const val DATA_SOCKET_PATH = "$BASE_URL"
-//const val VIDEO_STREAMING_PATH = "$BASE_URL/stream?topic=/camera_processing/camera/image_color/BGR/raw"
-
-//const val BASE_URL = "http://192.168.0.245:5170"
-
-
 const val KEY_URL = "aldajo92.URL"
 const val KEY_URL_LOCAL_IP = "aldajo92.URL_LOCAL"
 const val KEY_URL_REMOTE = "aldajo92.URL_REMOTE"
