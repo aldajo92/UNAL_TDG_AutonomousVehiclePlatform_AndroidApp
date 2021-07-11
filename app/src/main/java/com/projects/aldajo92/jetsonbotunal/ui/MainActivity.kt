02157a4b592d3762f4802f6b7f2b1dd1bfd38012
aldajo92 <alejodiscovery20@gmail.com>
2021-07-10 23:28:13 -0500

    improved ui colors

--- app/src/main/java/com/projects/aldajo92/jetsonbotunal/ui/MainActivity.kt
+++ app/src/main/java/com/projects/aldajo92/jetsonbotunal/ui/MainActivity.kt
@@ -1,42 +1,40 @@
 package com.projects.aldajo92.jetsonbotunal.ui
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.InputDevice
 import android.view.MotionEvent
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.niqdev.mjpeg.DisplayMode
 import com.github.niqdev.mjpeg.MjpegInputStream
 import com.projects.aldajo92.jetsonbotunal.R
 import com.projects.aldajo92.jetsonbotunal.api.SharedPreferencesManager
+import com.projects.aldajo92.jetsonbotunal.databinding.ActivityMainBinding
 import com.projects.aldajo92.jetsonbotunal.getVideoStreamingPath
 import com.projects.aldajo92.jetsonbotunal.models.MoveRobotMessage
 import com.projects.aldajo92.jetsonbotunal.ui.configuration.ConfigurationDialog
 import com.projects.aldajo92.jetsonbotunal.ui.views.MultiXYWrapper
-import kotlinx.android.synthetic.main.activity_main.floating_button_play
-import kotlinx.android.synthetic.main.activity_main.floating_settings_button
-import kotlinx.android.synthetic.main.activity_main.lineChart_control
-import kotlinx.android.synthetic.main.activity_main.progress_x
-import kotlinx.android.synthetic.main.activity_main.progress_y
-import kotlinx.android.synthetic.main.activity_main.videoView
-import kotlinx.android.synthetic.main.activity_main.view.progressbar_x_neg
-import kotlinx.android.synthetic.main.activity_main.view.progressbar_x_pos
-import kotlinx.android.synthetic.main.activity_main.view.progressbar_y_neg
-import kotlinx.android.synthetic.main.activity_main.view.progressbar_y_pos
-import kotlinx.android.synthetic.main.activity_main.viewPager
 import java.util.Timer
 import kotlin.concurrent.fixedRateTimer
 import kotlin.math.abs
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val realTimeWrapper by lazy {
         MultiXYWrapper.getInstance(
-            lineChart_control,
+            binding.lineChartControl,
             listOf(
                 ColorTemplate.getHoloBlue(),
                 Color.rgb(244, 10, 10)
@@ -54,29 +52,55 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        mainViewModel.connectSocket(sharedPreferencesManager.getSelectedBaseUrl() ?: "")
-
-        floating_settings_button.setOnClickListener {
-            val newFragment = ConfigurationDialog()
-            newFragment.show(supportFragmentManager, "configuration")
+        binding.floatingSettingsButton.setOnClickListener {
+            ConfigurationDialog().show(supportFragmentManager, "configuration")
         }
 
-        floating_button_play.setOnClickListener {
-            captureTimer = if (captureTimer == null) mainViewModel.runCaptureTimer()
-            else captureTimer?.let { it.cancel(); null }
-        }
+        binding.floatingButtonPlay.setOnClickListener { triggerCaptureTimer() }
+
+        binding.seekBarVelocity.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                maxValue = p1
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
 
         val pagerAdapter = ScreenSlidePagerAdapter(this)
-        viewPager.adapter = pagerAdapter
+        binding.viewPager.adapter = pagerAdapter
 
+    }
+
+    private fun triggerCaptureTimer() {
+        captureTimer = if (captureTimer == null) {
+            binding.floatingButtonPlay.backgroundTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(this, R.color.custom_red)
+            )
+            mainViewModel.runCaptureTimer()
+        } else {
+            binding.floatingButtonPlay.backgroundTintList = ColorStateList.valueOf(
+                getThemeAccentColor(this)
+            )
+            captureTimer?.let { it.cancel(); null }
+        }
     }
 
     override fun onPause() {
         super.onPause()
         try {
-            videoView.stopPlayback()
+            binding.videoView.stopPlayback()
+            mainViewModel.disconnectSocket()
         } catch (e: Exception) {
             Log.e("ADJ", "camera not opened")
         }
@@ -85,6 +109,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         startVideoStream()
+        mainViewModel.connectSocket(sharedPreferencesManager.getSelectedBaseUrl() ?: "")
     }
 
     private fun runCommandTimer(value: Any): Timer = fixedRateTimer(
@@ -99,7 +124,7 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mainViewModel.disconnectSocket()
-        videoView.stopPlayback()
+        binding.videoView.stopPlayback()
     }
 
     private var globalTimer: Timer? = null
@@ -109,6 +134,8 @@ class MainActivity : AppCompatActivity() {
     var ly = 0f
     var rx = 0f
 
+    var maxValue = 0
+
     override fun onGenericMotionEvent(event: MotionEvent): Boolean {
         return if (event.source and InputDevice.SOURCE_JOYSTICK == InputDevice.SOURCE_JOYSTICK && event.action == MotionEvent.ACTION_MOVE) {
             globalTimer?.cancel()
@@ -116,23 +143,23 @@ class MainActivity : AppCompatActivity() {
             val joyRx = event.getAxisValue(MotionEvent.AXIS_Z)
 
             // TODO: Move all related logic above to view model
-            ly = if (abs(joyLy) >= minMovement) -joyLy else 0f
+            ly = if (abs(joyLy) >= minMovement) (-joyLy * maxValue) / 100 else 0f
             rx = if (abs(joyRx) >= minMovement) -joyRx else 0f
 
             if (ly >= 0) {
-                progress_y.progressbar_y_pos.progress = (ly * 100).toInt()
-                progress_y.progressbar_y_neg.progress = 0
+                binding.progressbarYPos.progress = (ly * maxValue).toInt()
+                binding.progressbarYNeg.progress = 0
             } else {
-                progress_y.progressbar_y_pos.progress = 0
-                progress_y.progressbar_y_neg.progress = (-ly * 100).toInt()
+                binding.progressbarYPos.progress = 0
+                binding.progressbarYNeg.progress = (-ly * maxValue).toInt()
             }
 
             if (-rx >= 0) {
-                progress_x.progressbar_x_pos.progress = (-rx * 100).toInt()
-                progress_x.progressbar_x_neg.progress = 0
+                binding.progressbarXPos.progress = (-rx * 100).toInt()
+                binding.progressbarXNeg.progress = 0
             } else {
-                progress_x.progressbar_x_pos.progress = 0
-                progress_x.progressbar_x_neg.progress = (rx * 100).toInt()
+                binding.progressbarXPos.progress = 0
+                binding.progressbarXNeg.progress = (rx * 100).toInt()
             }
 
             val movementMessage = MoveRobotMessage(ly, rx)
@@ -154,19 +181,25 @@ class MainActivity : AppCompatActivity() {
         sharedPreferencesManager.getSelectedBaseUrl()?.let {
             mainViewModel.getMJPEJObserver(it.getVideoStreamingPath())
                 .subscribe({ inputStream: MjpegInputStream? ->
-                    videoView.setSource(inputStream)
-                    videoView.setDisplayMode(DisplayMode.BEST_FIT)
-                    videoView.showFps(true)
+                    binding.videoView.setSource(inputStream)
+                    binding.videoView.setDisplayMode(DisplayMode.BEST_FIT)
+                    binding.videoView.showFps(true)
                 }, {
                     Toast.makeText(
                         this, "error connecting to robot",
                         Toast.LENGTH_LONG
                     ).show()
-                    videoView.stopPlayback()
+                    binding.videoView.stopPlayback()
                 })
         }
 
-        videoView.setOnFrameCapturedListener(mainViewModel)
+        binding.videoView.setOnFrameCapturedListener(mainViewModel)
+    }
+
+    private fun getThemeAccentColor(context: Context): Int {
+        val value = TypedValue()
+        context.theme.resolveAttribute(R.attr.colorAccent, value, true)
+        return value.data
     }
 
 }
